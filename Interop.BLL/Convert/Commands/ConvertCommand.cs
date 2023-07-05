e819ce3185567f2504a43fd6ca60631c0e54089c
Polly <apollinaria.9203@yandex.by>
2023-07-05 03:29:40 +0300

    Initial commit

---
+++ Interop.BLL/Convert/Commands/ConvertCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Interop.BLL.Convert.Commands
+{
+    public class ConvertCommand : IRequest<string>
+    {
+        [JsonPropertyName("original_file_path")]
+        public string OriginalFilePath { get; set; }
+    }
+}
