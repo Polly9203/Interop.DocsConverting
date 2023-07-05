41f413df99741c5a24eef79755c07157bd55f8b6
Polly <apollinaria.9203@yandex.by>
2023-07-05 04:08:50 +0300

    Code refactoring (again)

--- Interop.PL/Program.cs
+++ Interop.PL/Program.cs
@@ -1,11 +1,9 @@
 using Interop.BLL;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 builder.Services.AddInteropBll();
 builder.Services.AddSwaggerGen();
 
